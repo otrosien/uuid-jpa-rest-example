ca918114599962505b57e96cf6fa62a065728ae4
Oliver Trosien <otrosien@epages.com>
2016-03-10 22:36:23 +0100

    showcase eclipselink with compile-time weaving

--- src/main/java/com/example/Mouse.java
+++ src/main/java/com/example/Mouse.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 
 import com.fasterxml.uuid.NoArgGenerator;
 
@@ -15,13 +14,12 @@ import lombok.Setter;
 @Entity
 public class Mouse {
 
-    @Transient
     private static final NoArgGenerator UUID_GENERATOR = DemoApplication.uuidGenerator();
 
     @Id
     @Column(columnDefinition="BINARY(16)")
     @Getter
-    private final UUID id;
+    private UUID id;
 
     @Getter
     @Setter
