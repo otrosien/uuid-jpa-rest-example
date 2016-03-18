2126d27aac2e7cf5cdeed3c95c4bf36c54330d69
Oliver Trosien <otrosien@epages.com>
2016-03-18 14:46:31 +0100

    revert eclipselink static weaving

--- src/main/java/com/example/Mouse.java
+++ src/main/java/com/example/Mouse.java
@@ -5,16 +5,20 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import com.fasterxml.uuid.NoArgGenerator;
 
 @Entity
 public class Mouse {
 
+    @Transient
     private static final NoArgGenerator UUID_GENERATOR = DemoApplication.uuidGenerator();
 
     @Id
     @Column(columnDefinition="BINARY(16)")
+    // JPA spec prohibits final, but 
+    // eclipselink and hibernate actually would be fine.
     private UUID id;
 
     private String name;
