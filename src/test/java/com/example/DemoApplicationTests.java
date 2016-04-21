bd7c55fec340b6f45ed809141443107ff1dd238d
Oliver Trosien <otrosien@epages.com>
2016-04-21 13:22:17 +0200

    some more behaviour testing

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example;
 
-import static org.hamcrest.core.StringEndsWith.endsWith;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -13,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -38,7 +42,7 @@ public class DemoApplicationTests {
     }
 
     @Test
-    public void restTest() throws Exception {
+    public void should_put_resource_and_get_it_via_location() throws Exception {
         mvc.perform(put("/mice/mickey").content(
                 new ObjectMapper().writeValueAsString(ImmutableMap.of(
                         "name", "mickey"
@@ -51,7 +55,46 @@ public class DemoApplicationTests {
             // follow the location header.
             mvc.perform(get(putMouse.getResponse().getHeader("Location")))
             .andDo(print())
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("name", is("mickey")))
+            .andExpect(jsonPath("id", notNullValue()));
+        });
+    }
+
+    @Test
+    public void should_not_update_mouse_id() throws Exception {
+        // given
+        mvc.perform(put("/mice/minney").content(
+                new ObjectMapper().writeValueAsString(ImmutableMap.of(
+                        "name", "minney",
+                        "id", "aa923a70-07b0-11e6-9f1b-a7ea756fd6b5"
+                        ))
+        ))
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(header().string("Location", endsWith("/mice/minney")))
+        .andDo((putMouse) -> {
+            // follow the location header.
+            mvc.perform(get(putMouse.getResponse().getHeader("Location")))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("id", is("aa923a70-07b0-11e6-9f1b-a7ea756fd6b5")));
+        })
+        .andDo((putMouse) -> {
+            // follow the location header. Have to use PUT as 
+            // on HTTP PATCH method there is no Location header.. ?!
+            mvc.perform(put(putMouse.getResponse().getHeader("Location"))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"name\":\"minney2\",\"id\":\"00000000-07b0-11e6-9f1b-a7ea756fd6b5\"}"))
+            .andDo(print())
+            .andExpect(status().isNoContent())
+            .andExpect(header().string("Location", endsWith("/mice/minney2")))
+            .andDo((patchMouse) -> {
+                mvc.perform(get(patchMouse.getResponse().getHeader("Location")))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id", is("aa923a70-07b0-11e6-9f1b-a7ea756fd6b5")));
+            });
         });
     }
 
