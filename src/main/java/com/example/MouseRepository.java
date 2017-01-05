b5c3221c3cd7a3cf6371797b0044e4d241884afc
Oliver Trosien <otrosien@epages.com>
2017-01-05 18:24:02 +0100

    fix build

--- src/main/java/com/example/MouseRepository.java
+++ src/main/java/com/example/MouseRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface MouseRepository extends PagingAndSortingRepository<Mouse, UUID> {
 
     @Query("SELECT m FROM Mouse m JOIN LatestMouse lm ON lm.id=m.id WHERE m.name=:name")
@@ -19,5 +17,4 @@ public interface MouseRepository extends PagingAndSortingRepository<Mouse, UUID>
     @Query("SELECT m FROM Mouse m JOIN LatestMouse lm ON lm.id=m.id")
     Page<Mouse> findAll(Pageable pageable);
 
-    Mouse findById(UUID id);
 }
